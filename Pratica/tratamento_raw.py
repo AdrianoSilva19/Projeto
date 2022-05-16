60f4094b9f2e76deb4fc582f449c07b460a0f825
Adriano Silva <ampsilva19rgmail.com>
2022-05-16 19:41:39 +0100

    [ADD] Lipid Maps transform

--- Pratica/tratamento_raw.py
+++ Pratica/tratamento_raw.py
@@ -5,41 +5,48 @@ import requests, zipfile, io
 
 # LIPID MAPS
 
+def extract():
+    """
+    This class calls the scrape_data method and creates a pandas dataframe with the scraped data.
+    :return:
+    """
+    df=_extract(scrape_data())
+    return df
+
+def _extract(raw) -> pd.DataFrame:
+    """
+    Method to create a pandas dataframe with the scraped data.
+    :return:
+    """
+    raw=PandasTools.LoadSDF(raw)
+    df=pd.DataFrame(raw)
+    print(df.head())
+    return df
+
 def scrape_data():
-    '''
+    """
     This class downloads the ZIP file and extracts the CSV files of lipid maps to a temporary folder.
     :return:
-    '''
+    """
     r = requests.get("https://www.lipidmaps.org/files/?file=LMSD&ext=sdf.zip")
     z = zipfile.ZipFile(io.BytesIO(r.content))
-    
-    return z.open('structures.sdf')  
 
+    return z.open('structures.sdf')
+def transform(df):
+    data=df[['LM_ID','ABBREVIATION','SYNONYMS']]
+    return data
 
-def _extract() -> pd.DataFrame:
-    """
-    #Method to create a pandas dataframe with the scraped data.
-    #:return:
-    """
-    raw=PandasTools.LoadSDF(scrape_data())
-    df=pd.DataFrame(raw)
-    df.to_csv('C:\\Users\\ampsi\\OneDrive\\Ambiente de Trabalho\\projeto\\Projeto\\Pratica\\lm_whole_db.csv',index=None)
-    return df
-    
-
-
-_extract()
+transform(extract())
 
 #-----------------------------------------------------------------------------------#
 
 # SWISS LIPIDS
-
-
+'''
 def scrape_data_sl():
-    """
-    This class downloads the ZIP file and extracts the CSV files of lipid maps to a temporary folder.
-    :return:
-    """
+    
+    #This class downloads the ZIP file and extracts the CSV files of lipid maps to a temporary folder.
+    #:return:
+    
     r = requests.get("https://www.swisslipids.org/api/file.php?cas=download_files&file=lipids.tsv")
     z = gzip.open(io.BytesIO(r.content),'rb')
     return z
@@ -48,14 +55,14 @@ def scrape_data_sl():
 
 
 def _extract_sl() -> pd.DataFrame:
-    """
-    Method to create a pandas dataframe with the scraped data.
-    :return:
-    """
+    
+    #Method to create a pandas dataframe with the scraped data.
+    #:return:
+    
     raw= pd.read_table(scrape_data_sl(),engine='python',encoding='ISO-8859-1')
     df=pd.DataFrame(raw)
-    df.to_csv('C:\\Users\\ampsi\\OneDrive\\Ambiente de Trabalho\\projeto\\Projeto\\Pratica\\sl_whole_db.csv',index=None)
+    print(df.head())
     return df
 
 _extract_sl()
-
+'''
